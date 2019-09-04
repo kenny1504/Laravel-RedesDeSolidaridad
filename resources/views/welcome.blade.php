708e3b5b531722092b740b8a872138edae87a74b
Kenny Saenz <Kennysaenz31@gmail.com>
2019-09-04 12:30:45 -0600

    Commit inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Redes De Solidaridads
+                   Redes De Solidaridad
                 </div>              
                 
             </div>
